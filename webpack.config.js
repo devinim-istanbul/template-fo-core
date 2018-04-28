ed657531d3af8248282e69751deb544135cc321b
Dinç Özdemir <dinc.ozdemir@emlakjet.com>
2018-04-28 17:28:58 +0300

    webpack 4 upgrade, functioning on web

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,16 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
     output: {
-        path: path.resolve(__dirname),
-        filename: 'index.js',
-        libraryTarget: 'this'
+        filename: "index.js",
+        library: "servemeFoCore",
+        libraryTarget: 'umd'
     },
-    externals: [nodeExternals()],
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        plugins: ["transform-runtime", "transform-object-rest-spread"],
-                        presets: [
-                            ["env", {
-                                "targets": {
-                                    "browsers": [
-                                        "last 2 versions",
-                                        "safari >= 9"
-                                    ],
-                                    "node": "current"
-                                }
-                            }]
-                        ]
-                    }
+                    loader: "babel-loader"
                 }
             }
         ]
